8d898b563edc35920b1d4d62334317c7d78c97f6
Akash Anil Kumar <akash.kumar@searce.com>
2022-11-16 11:22:14 +0530

    changes made in last exercises

--- 13-exercise-ninja-level/hands-on-ex3/mymath/main_test.go
+++ 13-exercise-ninja-level/hands-on-ex3/mymath/main_test.go
@@ -3,27 +3,35 @@ package mymath
 import ("testing"
 "fmt")
 
-func testavg(t *testing.T){
-	type avg struct{
-		data[] int ,
-		answer int,
+func TestCenteredAvg(t *testing.T){
+	type test struct{
+		data []int
+		answer float64
 	}
-	tests := test{
-		test([]int{10,20,30,40,},20)
-		test([]int {1,2,3,4,5},2)
+	tests := []test{test{[]int{10,20,30,40},25},
+	    test{[]int{1,2,3,4,},2.5,}}
+	for _,v :=range tests{
+		f := CenteredAvg(v.data)
+		if f != v.answer{
+                 t.Error("recieved",f,"expected",v.answer)
+		}
 	}
-	for _,v := range tests{
-             f := CenteredAvg(v.data)
-			 if f!= v.answer{
-				t.Error("recieved",f,"expected",v.data)
-			 }
-	}
-	func EgAverage(){
-		f:=CenteredAvg([]int )
-	}
-
-
 }
+
+func Egtestavg(){
+	fmt.Println(CenteredAvg([]int{10,20,30,40}),20)
+	//OUTPUT 
+	//20
+}
+
+func Benchavg(b *testing.B){
+	for i:=0;i<b.N;i++{
+		CenteredAvg([]int{100,200,300,400,500})
+	}
+}
+
+
+
 func bmtesting(b *testing.B){
 	for i:=0;i<b.N;i++{
 		CenteredAvg([]int{1,2,3,4,10000})
